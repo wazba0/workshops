add96607f8a96d1d4e2a589aead445b76908185e
wazba0 <aahmetarslnn@gmail.com>
2026-03-29 18:03:08 +0300

    prg_w1p2.c

---
+++ ws1/prg_w1p2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Workshop 1 Part-2");
+    printf("\n===============");
+    printf("\n\n Using tab specifiers...");
+    printf("\n\t Tab-1\tTab-2\tTab-3");
+    printf("\n\t-----\t=====\t-----");
+    printf("\n\n \\ this is a back-slash character!"); 
+    printf("\n %% this is a percent sign character!");
+    printf("\n \" this is a double-quote character!");
+    printf("\n\n My favourite quotes are:");
+    printf("\n1.\t \"Skill is only developed by hours and hours of work.\"");
+    printf("\n\t\t\t\t\t\t-Usain Bolt");
+    printf("\n\n2.\t \"It's not about having time. It's about making time.\"");
+    printf("\n\t\t\t\t\t\t-unknown");
+    printf("\n\n3.\t \"All of us do not have equal talent. But, all of us");
+    printf("\n\t have an equal opportunity to develop our talents.\"");
+    printf("\n\t\t\t\t\t\t-A.P.J Abdul Kalam");
+
+    return 0;
+}
