fc5e9dea70071bd059bf970306cf5807edb840f2
wazba0 <aahmetarslnn@gmail.com>
2026-03-29 18:02:33 +0300

    w1p1.c

---
+++ ws1/w1p1.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main(){
+	printf("Workshop 1 Part-1");
+	printf("\n==================");
+	printf("\n I'm displaying this using the 'printf' stdio");
+	printf("\n(standard input output) library function!");
+	printf("\n Dear Teacher");
+	printf("\n \t I promise I will work hard from this day onward.");
+	printf("\n \t I acknowledge that practive is extremely important,");
+	printf("\n \t so I will do all workshops, quizzes, and assigments.");
+	printf("\n Sincerely,");
+	printf("\n Ahmet Huseyin ARSLAN");
+
+	return 0; 
+}
+
