8fd50785f02d8e3dc0e370db970159cad64bb87d
wazba0 <aahmetarslnn@gmail.com>
2026-04-02 15:38:03 +0300

    w1p1.c

--- ws1/prg_w1p1.c
+++ ws1/prg_w1p1.c
@@ -1,17 +1,22 @@
 #include <stdio.h> 
 
-int main(){
+int main() {
+	char First_Name1[10] = "Ahmet";
+	char First_Name2[10] = "Huseyin";
+	char Last_Name[10] = "ARSLAN";
+	long long int student_ıd = 22220120070;
 	printf("Workshop 1 Part-1");
-	printf("\n==================");
-	printf("\n I'm displaying this using the 'printf' stdio");
+	printf("\n=================");
+	printf("\n\nI'm displaying this using the 'printf' stdio");
 	printf("\n(standard input output) library function!");
-	printf("\n Dear Teacher");
-	printf("\n \t I promise I will work hard from this day onward.");
-	printf("\n \t I acknowledge that practive is extremely important,");
-	printf("\n \t so I will do all workshops, quizzes, and assigments.");
-	printf("\n Sincerely,");
-	printf("\n Ahmet Huseyin ARSLAN");
+	printf("\n\nDear Teacher");
+	printf("\n\n\tI promise I will work hard from this day onward.");
+	printf("\n \tI acknowledge that practive is extremely important,");
+	printf("\n \tso I will do all workshops, quizzes, and assigments.\n");
+	printf("\nSincerely,\n");
+	printf("\n%s %s %s",First_Name1, First_Name2, Last_Name);
+	printf("\nStudent ID# %lld", student_ıd);
 
-	return 0; 
+	return 0;
 }
 
