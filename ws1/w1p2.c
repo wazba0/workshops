9f951cb164eabaac16f0ac8caefc6775dbf0956e
wazba0 <aahmetarslnn@gmail.com>
2026-04-02 15:45:19 +0300

    Update and rename prg_w1p2.c to w1p2.c

---
+++ ws1/w1p2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Workshop 1 Part-2");
+    printf("\n=================");
+    printf("\n\nUsing tab specifiers...");
+    printf("\n\tTab-1\tTab-2\tTab-3");
+    printf("\n\t-----\t=====\t-----");
+    printf("\n\n\\ this is a back-slash character!");
+    printf("\n%% this is a percent sign character!");
+    printf("\n\" this is a double-quote character!");
+    printf("\n\nMy favourite quotes are:");
+    printf("\n1.\t\"Skill is only developed by hours and hours of work.\"");
+    printf("\n\t\t\t\t\t\t\-Usain Bolt");
+    printf("\n\n2.\t\"It's not about having time. It's about making time.\"");
+    printf("\n\t\t\t\t\t\t-unknown");
+    printf("\n\n3.\t\"All of us do not have equal talent. But, all of us");
+    printf("\n\thave an equal opportunity to develop our talents.\"");
+    printf("\n\t\t\t\t\t\t-A.P.J Abdul Kalam");
+
+    return 0;
+}
